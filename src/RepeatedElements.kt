74e56509a4ac058a701d2675f5f0eef7709a8176
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-18 07:38:20 +0530

    Added files

--- src/RepeatedElements.kt
+++ src/RepeatedElements.kt
@@ -1,13 +1,25 @@
 fun main() {
     val arr = intArrayOf(20, -20, 30, 20, 50)
     PrintRepeated(arr)
+    val arr1 = intArrayOf(-2,-1,-2,1,2,33)
+    PrintRepeated(arr1)
 }
 
 fun PrintRepeated(arr: IntArray) {
-    for (i in arr.indices) {
-        for (j in i + 1 until arr.size)
-            if (arr[i] == arr[j]) {
-                print("" + arr[i] + ", ")
+    val list = arrayListOf<Int>()
+    for (i in arr) {
+        var count =0
+        for (j in arr) {
+            if (i == j) {
+                count++
+                if (count > 1) list.add(j)
             }
+        }
+    }
+    if (list.size==0) println("0")
+    else{
+        println(arr.contentToString())
+        for (i in list) print("$i ")
+        println()
     }
 }
